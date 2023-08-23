68aa9ddb2525c96efe9851a1a0fb57b15bcb89fb
Matt Halstead <m.russell.halstead@gmail.com>
2023-08-23 17:52:03 -0400

    Minor change to managers.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -37,7 +37,7 @@ export const managers = [
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Everyone", // Can be anything (usually your rival's name)
-        link: 6, // manager array number within this array, or null to link back to all managers page
+        link: null, // manager array number within this array, or null to link back to all managers page
         image: "/managers/MattHalstead.png", // either a specific manager photo or '/managers/everyone.png' or '/managers/question.png'
       },
       "favoritePlayer": 4037, // (optional) this corresponds to the Sleeper player ID (https://api.sleeper.app/v1/players/nfl)
